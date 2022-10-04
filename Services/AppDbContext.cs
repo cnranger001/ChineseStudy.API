576bb169b89e9bd1112579cf791bb459b5909b10
cnranger001 <1047860503@qq.com>
2022-10-04 21:20:16 +0800

    restore AppDbContext

---
+++ Services/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿﻿using System.Collections.Generic;
+using System.Net;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Entities
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Word> Words { get; set; }
+        public DbSet<Idiom> Idioms { get; set; }
+        public DbSet<Poem> Poems { get; set; }
+        public DbSet<Article> Articles { get; set; }
+
+        public DbSet<StudyHistory> StudyHistory { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //   optionsBuilder.UseSqlServer("Server = .; Database = ChineseStudy; Integrated Security=True");
+
+        }
+
+        public AppDbContext() : base()
+        {
+        }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+
+}
